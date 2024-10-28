a2324a05c00a9dae36c15be72d984589e5ca55e1
Saeed Habibi <178019482+saeed-habibi@users.noreply.github.com>
2024-10-28 22:29:53 +0330

    Create a basic menu and contact page using js

---
+++ src/createContactPage.js
@@ -0,0 +1,19 @@
+function createHeading() {
+  const boxOuter = document.createElement("div");
+  boxOuter.classList.add("box-outer", "heading");
+
+  const boxInner = document.createElement("div");
+  boxInner.classList.add("box-inner");
+
+  const h1 = document.createElement("h1");
+  h1.textContent = "Contact Page";
+
+  boxInner.append(h1);
+  boxOuter.append(boxInner);
+
+  return boxOuter;
+}
+
+export default function (contentBox) {
+  contentBox.append(createHeading());
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,53 @@
 import "./styles.css";
 import beeImage from "./assets/images/bee.svg";
 import createHomePage from "./createHomePage.js";
+import createMenuPage from "./createMenuPage.js";
+import createContactPage from "./createContactPage.js";
 
 // html handles
 const contentBox = document.querySelector("#content");
+const homeButton = document.querySelector("#home-button");
+const menuButton = document.querySelector("#menu-button");
+const contactButton = document.querySelector("#contact-button");
 
+// clear the page
+function clearContentBox() {
+  while (contentBox.hasChildNodes()) {
+    contentBox.removeChild(contentBox.firstChild);
+  }
+}
+
+function generateHomePage() {
+  clearContentBox();
+  createHomePage(contentBox, beeImage);
+
+  menuButton.classList.remove("active-button");
+  contactButton.classList.remove("active-button");
+  homeButton.classList.add("active-button");
+}
+
+function generateMenuPage() {
+  clearContentBox();
+  createMenuPage(contentBox);
+
+  contactButton.classList.remove("active-button");
+  homeButton.classList.remove("active-button");
+  menuButton.classList.add("active-button");
+}
+
+function generateContactPage() {
+  clearContentBox();
+  createContactPage(contentBox);
+
+  homeButton.classList.remove("active-button");
+  menuButton.classList.remove("active-button");
+  contactButton.classList.add("active-button");
+}
+
+// event listeners
+homeButton.addEventListener("click", generateHomePage);
+menuButton.addEventListener("click", generateMenuPage);
+contactButton.addEventListener("click", generateContactPage);
+
+// run at page load
 createHomePage(contentBox, beeImage);
